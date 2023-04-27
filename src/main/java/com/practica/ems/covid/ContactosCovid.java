11a028ad8bcc907e4a50c9dc6bda024c6777009e
m.khudzey <m.khudzey@alumnos.upm.es>
2023-04-27 10:21:10 +0200

    complejidad ciclomatica contactos covid

--- src/main/java/com/practica/ems/covid/ContactosCovid.java
+++ src/main/java/com/practica/ems/covid/ContactosCovid.java
@@ -120,16 +120,13 @@ public class ContactosCovid {
 				datas = dividirEntrada(data.trim());
 				for (String linea : datas) {
 					String datos[] = this.dividirLineaData(linea);
-					if (!datos[0].equals("PERSONA") && !datos[0].equals("LOCALIZACION")) {
-						throw new EmsInvalidTypeException();
-					}
 					if (datos[0].equals("PERSONA")) {
 						if (datos.length != Constantes.MAX_DATOS_PERSONA) {
 							throw new EmsInvalidNumberOfDataException("El número de datos para PERSONA es menor de 8");
 						}
 						this.poblacion.addPersona(this.crearPersona(datos));
 					}
-					if (datos[0].equals("LOCALIZACION")) {
+					else if (datos[0].equals("LOCALIZACION")) {
 						if (datos.length != Constantes.MAX_DATOS_LOCALIZACION) {
 							throw new EmsInvalidNumberOfDataException(
 									"El número de datos para LOCALIZACION es menor de 6" );
@@ -137,6 +134,9 @@ public class ContactosCovid {
 						PosicionPersona pp = this.crearPosicionPersona(datos);
 						this.localizacion.addLocalizacion(pp);
 						this.listaContactos.insertarNodoTemporal(pp);
+					}
+					else {
+						throw new EmsInvalidTypeException();
 					}
 				}
 
